c692d4b30c6a7d3a0be2c70b6488319bfeed84d4
Suman Das <40028942+sumanxd@users.noreply.github.com>
2018-07-24 15:44:02 +0530

    Add files via upload

---
+++ data/comments-data.js
@@ -0,0 +1,23 @@
+var commentsArray = [  
+{  
+   "id": 1,
+   "parent": null,
+   "created": "2018-07-24",
+   "content": "Hello js",
+   "pings": [],
+   "creator": 6,
+   "fullname": "Suman Das",
+   "created_by_admin": false,
+   "created_by_current_user": false,
+   "user_has_upvoted": false,
+   "is_new": false
+}
+]
+
+var usersArray = [
+   {
+      id: 1,
+      fullname: "Current User",
+      email: "itsmetoton@gmail.com"
+   }
+]
